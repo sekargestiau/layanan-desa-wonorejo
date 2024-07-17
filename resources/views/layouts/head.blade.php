bc88d661f171005933e98e32aa5878b8f6b17031
sekargestiau <sekargestiau@gmail.com>
2024-07-17 20:36:46 +0700

    update layout

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,15 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!--====== Favicon Icon ======-->
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="assets/img/logo_wonorejo.png" type="image/png">
 
-    <title>Shine - TailwindCSS Startup and SaaS Landing Page Template</title>
-
-    <!-- Icon -->
-    <link rel="stylesheet" type="text/css" href="assets/css/LineIcons.2.0.css">
-    <!-- Animate -->
-    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
-    <!-- Tiny Slider  -->
-    <link rel="stylesheet" type="text/css" href="assets/css/tiny-slider.css">
-    <!-- Tailwind css -->
-    <link rel="stylesheet" type="text/css" href="assets/css/tailwind.css">
+    <title>Posyandu Desa Wonorejo</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-      extend: {},
+    extend: {},
   },
-  plugins: [],
-};
+  plugins: [
+      require('flowbite/plugin')
+  ],
+}
