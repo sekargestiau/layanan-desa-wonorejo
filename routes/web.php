6f8e73614b7fb1ea513b2d46958ab282b10e5a1e
Aditia Nugraha <90129489+Adztrz@users.noreply.github.com>
2024-07-18 11:03:00 +0700

    Added Agenda.Index

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ Route::get('/peta', function(){
     $title = 'Map';
     return view('map.index', compact('title'));
 });
+
+Route::get('/agenda', function(){
+    $title = 'Map';
+    return view('agenda.index', compact('title'));
+});
