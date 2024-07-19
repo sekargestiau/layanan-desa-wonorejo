f426f188636c95a0ff5eca2cbb92c14b6a0fa436
Rio Saputro <rio.ae23@gmail.com>
2024-07-19 10:12:18 +0700

    Layout Sidebar, Routing Group

--- resources/views/map/admin/index.blade.php
+++ resources/views/map/admin/index.blade.php
@@ -10,9 +10,10 @@
  <div class="p-4 sm:ml-64">
    
 
+     <button class="bg-blue-200 hover:bg-blue-300 font-bold text-blue-600 px-3 py-2 rounded-md mb-6"> Tambah Data </button>
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-black uppercase bg-blue-300 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     Dusun
@@ -47,7 +48,24 @@
                     -7.42142, 118.2321
                 </td>
                 <td class="px-6 py-4">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <a href="#" class="font-medium bg-blue-200 px-5 py-2 hover:bg-blue-300 rounded-lg text-blue-600 dark:text-blue-500 ">Edit</a>
+                </td>
+            </tr>
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Blimbing
+                </th>
+                <td class="px-6 py-4">
+                    1
+                </td>
+                <td class="px-6 py-4">
+                    1
+                </td>
+                <td class="px-6 py-4">
+                    -7.42142, 118.2321
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium bg-blue-200 px-5 py-2 hover:bg-blue-300 rounded-lg text-blue-600 dark:text-blue-500 ">Edit</a>
                 </td>
             </tr>
         </tbody>
--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $title = 'Map';
-    return view('index',
-compact('title'));
+    return view(
+        'index',
+        compact('title')
+    );
 });
 
-Route::get('/peta', function(){
-    $title = 'Map';
-    return view('map.index', compact('title'));
+Route::group(['prefix' => 'peta'], function () {
+    Route::get('/', function () {
+        $title = 'Map';
+        return view('map.index', compact('title'));
+    });
+    Route::get('/admin', function () {
+        $title = 'Map';
+        return view('map.admin.index', compact('title'));
+    });
 });
 
-Route::get('/peta/admin', function(){
-    $title = 'Map';
-    return view('map.admin.index', compact('title'));
-});
 
-Route::get('/agenda', function(){
+Route::get('/agenda', function () {
     $title = 'Map';
     return view('agenda.index', compact('title'));
 });
 
-Route::get('/navTest', function(){
+Route::get('/navTest', function () {
     $title = 'Map';
     return view('map.navbar', compact('title'));
 });
